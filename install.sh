8a86dbdb321499c7129cec8d0d00bf22bbf711c5
Daniel Martinez <Zerobim@users.noreply.github.com>
2020-10-22 21:29:33 +0200

    Changed dangerous install instructions

--- install.sh
+++ install.sh
@@ -2,9 +2,4 @@
 out_file='install.out'
 err_file='install.err'
 
-pacman -Sy --noconfirm git
-git clone 'https://github.com/Zerobim/arch-install.git'
-cd arch-install
-git checkout dev
-
 ./run_in_live.sh 2>"$err_file" | tee "$out_file"
